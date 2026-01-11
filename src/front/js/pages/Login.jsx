4a436d758d2c17b841cd2f138276a06d8251009e
Asiiz127 <alexmartintv@gmail.com>
2026-01-11 13:54:58 +0100

    ajustes en el css del formulario de login

--- src/front/js/pages/Login.jsx
+++ src/front/js/pages/Login.jsx
@@ -2,6 +2,7 @@ import "../../styles/login.css";
 import LoginHeader from "../component/LoginHeader";
 import LoginForm from "../component/LoginForm";
 import SocialLogin from "../component/SocialLogin";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
     return (
@@ -13,7 +14,7 @@ export const Login = () => {
             <div className="login-footer">
                 <p>
                   ¿Nuevo en la ruta?
-                  <span className="login-link"> Crear una cuenta</span>
+                  <Link to="/signup" className="login-link"> Crea una cuenta</Link>
                 </p>
             </div>
         </main>
